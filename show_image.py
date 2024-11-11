dcac17d26f0bb36bda1716d193a4b4796166fb71
Guilherme Nobre <guinobre1602@gmail.com>
2024-11-11 09:39:11 -0300

    first commit

---
+++ show_image.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import cv2
+
+def show_image(image):
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+
+def main():
+    
+    return
+
+if __name__ == "__main__":
+    main()
